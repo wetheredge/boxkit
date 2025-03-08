efc17293a758d84e968304a05174788e9b8d671f
Wren Etheredge <me@wetheredge.com>
2025-03-07 22:51:22 -0500

    Add atim/starship copr

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -3,6 +3,8 @@
 # Symlink distrobox shims
 ./distrobox-shims.sh
 
+grep -v '^#' ./coprs | xargs -n 1 dnf copr enable -y
+
 # Update the container and install packages
 dnf update -y
 grep -v '^#' ./packages | xargs dnf install -y
